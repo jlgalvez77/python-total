d37c8d3cae0ad5bd1139f3ce22949b354e5e9216
Jose Galvez <jlgalvez77@gmail.com>
2025-01-09 22:30:24 +0100

    Dia 1

---
+++ Dia 1/Primer_Proyecto.py
@@ -0,0 +1,7 @@
+print('Hola Python')
+print("Hola " + 'Python')
+print('Mi nombre es \"Hammer\"')
+
+name = input('Cual es tu nombre? ')
+lastname = input('Cual es tu apellido? ')
+print('Hola ' + name + ' ' + lastname)
---
+++ Dia 1/proyecto.py
@@ -0,0 +1,3 @@
+question1 = input('Escribe un planeta: ')
+question2 = input('Escribe un coloe: ')
+print(f'La cerveza se llama {question1} {question2}')
