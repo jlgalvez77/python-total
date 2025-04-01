75701c4231f7fb3633af4632a6e1c8316b26ab76
Jose Galvez <jlgalvez@gmail.com>
2025-04-01 18:25:41 +0200

    Dia 6.1

---
+++ Dia 6/Proyecto/main.py
@@ -0,0 +1,8 @@
+while True:
+    print('''Selecciona una opción: 
+    1.- Leer receta.
+    2.- Crear receta.
+    3.- Crear categoría.
+    4.- Eliminar receta.
+    5.- Elimminar categoria.
+    6.- Finalizar programa.''')
